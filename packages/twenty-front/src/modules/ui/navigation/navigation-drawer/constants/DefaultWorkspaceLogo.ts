0106b5da8473f4fde9e88fe79752e3aac835c922
zrmnelson <zacharynelson1@gmail.com>
2026-07-13 10:51:49 +0530

    style(front): satisfy workspace logo lint rule

--- packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
+++ packages/twenty-front/src/modules/ui/navigation/navigation-drawer/constants/DefaultWorkspaceLogo.ts
@@ -4,9 +4,10 @@ import { getAbsoluteImageUrl } from '~/utils/image/getAbsoluteImageUrl';
 
 export const DEFAULT_WORKSPACE_LOGO = '/images/brand/myah-mark.png';
 
-export const getWorkspaceLogoUrl = (
+export function getWorkspaceLogoUrl(
   workspaceLogo?: string | null,
-): string =>
-  isNonEmptyString(workspaceLogo)
+): string {
+  return isNonEmptyString(workspaceLogo)
     ? (getAbsoluteImageUrl(workspaceLogo) ?? DEFAULT_WORKSPACE_LOGO)
     : DEFAULT_WORKSPACE_LOGO;
+}
