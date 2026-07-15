802218a7b77f74234ba98ced10b27227b7238c31
daryllnetscher <daryll@netscher.com>
2026-07-15 18:32:17 +0700

    feat(metadata): Add typed Myah standard roles

--- packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/role-metadata/create-standard-flat-role-metadata.util.ts
+++ packages/twenty-server/src/engine/workspace-manager/twenty-standard-application/utils/role-metadata/create-standard-flat-role-metadata.util.ts
@@ -1,5 +1,8 @@
+import {
+  buildMyahBrandBrainAdminStandardFlatRole,
+  buildMyahCreatorOpsDefaultStandardFlatRole,
+} from 'src/engine/workspace-manager/twenty-standard-application/utils/role-metadata/myah-standard-role-builders.util';
 import { type FlatRole } from 'src/engine/metadata-modules/flat-role/types/flat-role.type';
-import { type AllStandardRoleName } from 'src/engine/workspace-manager/twenty-standard-application/types/all-standard-role-name.type';
 import {
   type CreateStandardRoleArgs,
   createStandardRoleFlatMetadata,
@@ -26,8 +29,9 @@ export const STANDARD_FLAT_ROLE_METADATA_BUILDERS_BY_ROLE_NAME = {
         canBeAssignedToApiKeys: true,
       },
     }),
-} satisfies {
-  [P in AllStandardRoleName]: (
-    args: Omit<CreateStandardRoleArgs, 'context'>,
-  ) => FlatRole;
-};
+  brandBrainAdmin: buildMyahBrandBrainAdminStandardFlatRole,
+  creatorOpsDefault: buildMyahCreatorOpsDefaultStandardFlatRole,
+} satisfies Record<
+  string,
+  (args: Omit<CreateStandardRoleArgs, 'context'>) => FlatRole
+>;
